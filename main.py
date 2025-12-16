751ed0ed335e2e8bea9c51deac9282d11eb35632
Liam_Fitz <liam6fitzgerald@outlook.com>
2025-12-16 10:48:23 +0100

    variables

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import pygame as pyg # importer le module pygame (game engine)
 import time
+from variables import * # importer les variables
 pyg.font.init() # initialiser le module font de pygame
-
-
-WIDTH, HEIGHT = 800, 600 # dimensions de la fenetre
-WIN = pyg.display.set_mode((WIDTH, HEIGHT)) # creer la fenetre
-pyg.display.set_caption("Player Movement Example") 
-
-BG = pyg.transform.scale(pyg.image.load("background.jpg"), (WIDTH, HEIGHT)) # charger et redimensionner le fond d'ecran
-
-
-PLAYER_WIDTH, PLAYER_HEIGHT = 25, 45 # taille du joueur
-PLAYER_VIT = 5 # vitesse de deplacement du joueur
-PLAYER_PV = 10 # points de vie du joueur
-MONSTER_VIT= 1
-
-FONT = pyg.font.SysFont("comicsans", 30) # definir la police d'ecriture
 
 
 def draw(player, temp_ecoulé, monster): # dessiner tout
