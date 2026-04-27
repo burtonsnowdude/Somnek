03485eb8dba9c59b1cd42070ebe66e57dec659da
fee21-dev <feerosekooh@gmail.com>
2026-04-27 06:05:46 +0000

    Modification de l'interface

--- Code/Interface.py
+++ Code/Interface.py
@@ -1,138 +1,260 @@
+from turtle import position
+from option import settings, flashing_effect, black_and_white
 import pygame as pyg
 import sys
-import random
-import subprocess
+
 from variables import *
+from realisation_brouillon import realisation_brouillon, POPUP_W, POPUP_H
+from Power_up_shop import open_shop
+from option import options, checkboxes
+from class_Button import Button
+import variable_power_up as data
+from collection import open_collection
+from start import open_start
+from collection import selected_item
+from main import main
 
 pyg.init()
+pyg.font.init()
+pyg.mixer.init()
 
-# Fenêtre
 WIN = pyg.display.set_mode((WIDTH, HEIGHT))
 pyg.display.set_caption("SOMNEK")
 
-# Fond d'écran
-fond_decran = pyg.image.load("Images/Interface/fond_accueil.png")
-fond_decran = pyg.transform.scale(fond_decran, (WIDTH, HEIGHT))
+fond_intro = pyg.image.load("Images/press.png")
+fond_intro = pyg.transform.scale(fond_intro, (WIDTH, HEIGHT))
 
-fond_enter = pyg.image.load("Images/Interface/press.png")
-fond_enter = pyg.transform.scale(fond_enter, (WIDTH, HEIGHT))
+fond_menu = pyg.image.load("Images/fond_accueil.png")
+fond_menu = pyg.transform.scale(fond_menu, (WIDTH, HEIGHT))
+
+fond_collection = pyg.image.load("Images/Collection.png")
+fond_collection = pyg.transform.scale(fond_collection, (350, 400))
+
+fond_credits = pyg.image.load("Images/fond_credits.png")
+fond_credits = pyg.transform.scale(fond_credits, (350, 400))
+
+choice_1 = pyg.image.load("Images/choice-1.png")
+choice_1 = pyg.transform.scale(choice_1, (350, 400))
+
+game = False
+show_image = False
+show_realisation = False
+show_credits = False
+show_start = False
+show_options = False  # Ajoutez cette variable
+show_shop = False
 
 
 
-# Musique
-"""pyg.mixer.music.load("Musique/Music_interface.mp3")"""
-"""pyg.mixer.music.play(-1)"""
+musique_bout = False
+musique_load = False
+musique_close = False
+musique_back = False
 
-# Couleurs
-WHITE = (255, 255, 255)
-TRANSLUCENT_BLUE = (0, 0, 255, 128)
-HOVER_BLUE = (0, 0, 200)
-SHADOW = (0, 0, 0, 100)
-game_enter = False
-# Polices
+musique_bout_played = False
+musique_load_played = False
+musique_close_played = False
+musique_back_played = False
+
+def play_music(path):
+    pyg.mixer.music.stop()
+    pyg.mixer.music.load(path)
+    pyg.mixer.music.play(0)
+
 try:
-    FONT_TITLE = pyg.font.Font("assets/pixels.ttf",130)
+    FONT_TITLE = pyg.font.Font("assets/pixels.ttf", 130)
     FONT_BUTTON = pyg.font.Font("assets/pixels.ttf", 36)
 except:
     FONT_TITLE = pyg.font.SysFont(None, 130)
     FONT_BUTTON = pyg.font.SysFont(None, 36)
 
-# Classe Button
-class Button:
-    def __init__(self, text, action, x,y, width, height):
-        self.text = text
-        self.action = action
-        self.width = width
-        self.height = height
-        self.rect = pyg.Rect(0, 0, self.width, self.height)
-        self.rect.center = (x, y)
+FONT_BUTTON = pyg.font.SysFont(None, 36)
 
-    def draw(self, win, mouse_pos):
-        hovered = self.rect.collidepoint(mouse_pos)
-        color = HOVER_BLUE if hovered else TRANSLUCENT_BLUE
-        button_surface = pyg.Surface((self.width, self.height), pyg.SRCALPHA)
-        pyg.draw.rect(button_surface, color, (0, 0, self.width, self.height), border_radius=10)
-        win.blit(button_surface, self.rect)
 
-        # Texte
-        shadow = FONT_BUTTON.render(self.text, True, SHADOW)
-        text_surface = FONT_BUTTON.render(self.text, True, WHITE)
-        text_rect = text_surface.get_rect(center=self.rect.center)
-        win.blit(shadow, (text_rect.x + 2, text_rect.y + 2))
-        win.blit(text_surface, text_rect)
+buttons = [
+    Button("COMMENCER", "load", 400, 305, 300, 65, FONT_BUTTON),
+    Button("COLLECTION", "collection", 150, 440, 220, 60, FONT_BUTTON),
+    Button("POWER UP", "power_up", 400, 440, 250, 70, FONT_BUTTON),
+    Button("REALISATIONS", "realisations", 670, 440, 260, 60, FONT_BUTTON),
+    Button("Credits", "credits", 400, 520, 200, 50, FONT_BUTTON),
+    Button("Options", "options", 100, 50, 180, 45, FONT_BUTTON),
+    Button("QUITTER", "quit", 700, 50, 180, 45, FONT_BUTTON),
+]
 
-    def is_clicked(self, mouse_pos, mouse_pressed):
-        return self.rect.collidepoint(mouse_pos) and mouse_pressed[0]
-def lancer_interface():
-    # Création des boutons  
-    buttons = [ 
-        Button("COMMENCER", "load", 400, 305, 300, 65),   
+btn_rev_coll = Button("X", "rev_coll", 550, 130, 40, 40, FONT_BUTTON)
+btn_rev_real = Button("X", "rev_real", 550, 130, 40, 40, FONT_BUTTON)
+btn_rev_cre = Button("X", "rev_cre", 550, 130, 40, 40, FONT_BUTTON)
+btn_rev_opt = Button("X", "rev_opt", 550, 130, 40, 40, FONT_BUTTON)  # Bouton pour fermer les options
+btn_confirm_nerd = Button("Confirmer", "confirm", 550, 500, 80, 40, FONT_BUTTON)
+btn_rev_start = Button("X", "rev_start", 550, 130, 40, 40, FONT_BUTTON)
 
-        Button("COLLECTION", "collection", 150, 440, 220, 60), 
-        Button("POWER UP", "power_up", 400, 440, 250, 70), 
-        Button("REALISATIONS", "realisations", 670, 440, 260, 60), 
 
-        Button("Credits", "credits", 400, 520, 200, 50), 
-        Button("Options", "options", 100, 50, 180, 45),   
-        Button("QUITTER", "quit", 700, 50, 180, 45),       
-        Button("Pressez ESPACE pour commencer", "start", 400, 300, 300, 50)]
-    # Initialiser la police
-    font = pyg.font.Font(None, 36)
-    # Initialiser le score
-    argent = 0
-    # Boucle principale
-    running = True
-    clock = pyg.time.Clock()
+argent = 0
+reset_clicked = False
+clock = pyg.time.Clock()
+running = True
 
-    while running:
-        clock.tick(60)
-        WIN.blit(fond_decran, (0, 0))
-        mouse_pos = pyg.mouse.get_pos()
-        mouse_pressed = pyg.mouse.get_pressed()
 
-        # Titre
-        title_shadow = FONT_TITLE.render("SOMNEK", True, SHADOW)
-        title = FONT_TITLE.render("SOMNEK", True, WHITE)
-        WIN.blit(title_shadow, (210, 165))  
-        WIN.blit(title, (210, 150)) 
+while running:
+    clock.tick(60)
 
-        # Boutons
+    mouse_pos = pyg.mouse.get_pos()
+    mouse_pressed = pyg.mouse.get_pressed()
+    events = pyg.event.get()
+
+    # RESET AUDIO FLAGS CHAQUE FRAME
+    musique_bout_played = False
+    musique_load_played = False
+    musique_close_played = False
+
+    if not game:
+        WIN.blit(fond_intro, (0, 0))
+        texte = FONT_BUTTON.render("Pressez ESPACE", True, (255, 255, 255))
+        WIN.blit(texte, (250, 300))
+
+    else:
+        WIN.blit(fond_menu, (0, 0))
+
+        title = FONT_TITLE.render("SOMNEK", True, (255, 255, 255))
+        WIN.blit(title, (210, 150))
+
+        argent_text = FONT_BUTTON.render(f"Argent: {data.player_money}", True, (0, 0, 0))
+        WIN.blit(argent_text, (330, 50))
+
         for btn in buttons:
             btn.draw(WIN, mouse_pos)
+
             if btn.is_clicked(mouse_pos, mouse_pressed):
                 pyg.time.delay(200)
 
-                if btn.action == "load":
-                    print("Commencer")
-                elif btn.action == "collection":
-                    print("Collection")     
-                elif btn.action == "credits":
-                    print("Credits")                
-                elif btn.action == "power_up":
-                    print("Power Up")
-                elif btn.action == "realisations":
-                    print("Réalisations")   
-                elif btn.action == "options":
-                    print("Options")
-                elif btn.action == "quit":  
-                    running = False
-    
-        
-        # Afficher l'argent
-        argent_text = font.render(f"Argent: {argent}", True, (0, 0, 0))
-        argent_rect = argent_text.get_rect(center=(400, 50))  # centre haut
-        WIN.blit(argent_text, argent_rect)
-    
+                if btn.action == "collection":
+                    show_image = True
+                    musique_bout = True
 
-        # Événements
-        for event in pyg.event.get():
+                elif btn.action == "options":
+                    show_options = True  # Afficher les options
+                    musique_bout = True
+
+                elif btn.action == "quit":
+                    musique_close = True
+                    running = False
+
+                elif btn.action == "realisations":
+                    show_realisation = True
+                    musique_bout = True
+
+                elif btn.action == "credits":
+                    show_credits = True
+                    musique_bout = True
+
+                elif btn.action == "load":
+                    musique_load = True
+                    show_start = True
+                    
+
+                elif btn.action == "power_up":
+                    musique_bout = True
+                    if btn.action == "power_up":
+                        show_shop = True
+        if not settings["music"]:
+            pyg.mixer.music.stop()
+
+        if musique_bout and not musique_bout_played:
+            if settings["sound"]:
+                play_music("Musique/sound_button.mp3")
+                musique_bout = False
+
+        if musique_load and not musique_load_played:
+            if settings["sound"]:
+                play_music("Musique/start.mp3")
+                musique_load = False
+
+        if musique_close and not musique_close_played:
+            if settings["sound"]:
+                    play_music("Musique/rev.mp3")
+                    musique_close = False
+        if settings["music"]:
+            if pyg.mixer.music.get_busy() == False:
+                pyg.mixer.music.load("Musique/music_interface.mp3")
+                pyg.mixer.music.play(-1)
+
+        if show_realisation:
+            popup = realisation_brouillon(events)
+
+            WIN.blit(
+                popup,
+                ((WIDTH - POPUP_W)//2, (HEIGHT - POPUP_H)//2)
+            )
+
+            btn_rev_real.draw(WIN, mouse_pos)
+
+            if btn_rev_real.is_clicked(mouse_pos, mouse_pressed):
+                pyg.time.delay(200)
+                show_realisation = False
+                musique_close = True
+
+        if show_credits:
+            WIN.blit(fond_credits, (240, 150))
+            btn_rev_cre.draw(WIN, mouse_pos)
+
+            if btn_rev_cre.is_clicked(mouse_pos, mouse_pressed):
+                pyg.time.delay(200)
+                show_credits = False
+                musique_close = True
+                
+
+        if show_start:
+            result = open_start(WIN, events, mouse_pos, mouse_pressed, btn_rev_start, FONT_BUTTON)
+
+            if result == "close":
+                show_start = False
+
+            if result == "start_game":
+                show_start = False
+                from main import main
+                main(WIN)
+                running = False
+
+        # AFFICHER LES OPTINS
+        if show_options:
+            # Vérifiez que events contient les bonnes données
+            options(events, mouse_pos, mouse_pressed, WIN)
+            
+            btn_rev_opt.draw(WIN, mouse_pos)
+
+            if btn_rev_opt.is_clicked(mouse_pos, mouse_pressed):
+                pyg.time.delay(200)
+                show_options = False
+                musique_close = True
+        if show_shop:
+            from Power_up_shop import open_shop, buy_selected
+
+            close = open_shop(events, WIN, mouse_pos, mouse_pressed, btn_rev_opt, FONT_BUTTON)
+
+            if btn_confirm_nerd.is_clicked(mouse_pos, mouse_pressed):
+                buy_selected()
+
+            if close:
+                show_shop = False
+        if show_image:
+                close = open_collection(events, WIN, mouse_pos, mouse_pressed, btn_rev_coll)
+
+                if close:
+                    show_image = False
+                    musique_close = True
+        
+
+    for event in events:
             if event.type == pyg.QUIT:
                 running = False
             if event.type == pyg.KEYDOWN:
                 if event.key == pyg.K_SPACE:
-                    print("Espace pressé - Lancer le jeu")
-                    game_enter = True
+                    game = True
 
-        pyg.display.flip()
+    if settings["bw"]:
+        black_and_white(WIN)
 
-    pyg.quit()
+    if settings["vfx"]:
+        flashing_effect(WIN)
+    pyg.display.flip()
+
